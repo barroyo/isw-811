511bc7a66711505a8771d8d3d0c83e2f8e8a25f6
Bladimir Arroyo <bladimir.ab@gmail.com>
2019-02-11 20:26:20 -0600

    Add basic auth

--- app.js
+++ app.js
@@ -1,36 +1,73 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const db = mongoose.connect("mongodb://127.0.0.1:27017/todo-api");
-const Task = require("./models/taskModel");
 const app = express();
-// const cors = require('cors');
+const Task = require("../todo-api-express/models/taskModel");
+const basicAuth = require('express-basic-auth')
+const {
+  base64decode
+} = require('nodejs-base64');
+
+const {
+  postMethod,
+  getMethod
+} = require("./controllers/todoController.js");
+const cors = require("cors");
 
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+// check for cors
+app.use(cors());
+
+// custom basic authentication
+app.use(function (req, res, next) {
+  if (req.headers["authorization"]) {
+    const authBase64 = req.headers['authorization'].split(' ');
+    const userPass = base64decode(authBase64[1]);
+    const user = userPass.split(':')[0];
+    const password = userPass.split(':')[1];
+
+    //
+    if (user === 'admin' && password == '1234') {
+      next();
+      return;
+    }
+
+    res.status(401);
+    res.send({
+      error: "Unauthorized "
+    });
+    return;
+  }
+});
+
+// using basic auth
+// app.use(basicAuth({
+//   users: {
+//     'admin': '1234',
+//     'user1': 'supersecret1',
+//     'user2': 'supersecret2',
+//   }
+// }));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(bodyParser.json());
-// app.use(cors());
 
 // // app.set('view engine', 'pug');
 // // // this is where we can put the public contents like css and js files
 // // app.use(express.static(__dirname + '/public'));
 
-app.get('/', (request, response) => {
-  //connect to db and get data
-  if (request.header['accept'] === 'text/json') {
-    response.json({
-      title: 'Main Title',
-      content: 'The content of the page'
-    })
-  } else {
-    response.render('index', {
-      title: 'Main Title',
-      content: 'The content of the page'
-    })
-  }
-});
+// // app.get('/', (request, response) => {
+// //     //connect to db and get data
+// //     response.render('index', {
+// //         title: 'Main Title',
+// //         content: 'The content of the page'
+// //     })
+// // });
 
 // app.get('/tasks', (request, response) => {
 //     //connect to db and get data
@@ -45,40 +82,9 @@ app.get('/', (request, response) => {
 //     });
 // });
 
-app.get("/api/tasks", (req, res) => {
-  Task.find((err, tasks) => {
-    if (err) {
-      console.log(err);
-      res.send({
-        erroCode: 2002,
-        message: 'There was an error'
-      });
-    }
-    res.json(tasks);
-  });
-});
-
-app.post("/api/tasks", (req, res) => {
-  var task = new Task();
-
-  task.title = req.body.title;
-  task.detail = req.body.detail;
-
-  task.save(function (err) {
-    if (err) {
-      res.status(422);
-      console.log('error while saving the task', err)
-      res.json({
-        error: 'There was an error saving the task'
-      });
-    }
-    res.status(201);
-    res.header({
-      'location': `http://localhost/api/tasks/${task.id}`
-    });
-    res.json(task);
-  });
-});
+// handle the routes
+app.get("/api/tasks", getMethod);
+app.post("/api/tasks", postMethod);
 
 // handle 404
 app.use(function (req, res, next) {
